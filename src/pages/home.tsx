dbc1b2dd3527c6c8cc62ef431380e7163cc3e245
ryandvl <contatoryandvl55@gmail.com>
2025-01-24 16:40:26 -0300

    feat: useSearchParams function

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,6 +5,7 @@ export function Home() {
 
       <nav>
         <a href="/products">Products</a>
+        <a href="/products?category=tvs">Categories</a>
       </nav>
     </div>
   );
--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,9 +1,21 @@
+import { useSearchParams } from "react-router";
+
 export function Products() {
+  const [searchParams] = useSearchParams();
+
+  const category = searchParams.get("category");
+
   return (
     <div>
       <a href="/">Back</a>
 
       <h1>Products</h1>
+
+      {category && (
+        <span>
+          Category <strong>{category}</strong>
+        </span>
+      )}
     </div>
   );
 }
