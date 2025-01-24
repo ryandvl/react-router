e1ab03f7d97b3104c7212415ce18c6adf2e1ff76
ryandvl <contatoryandvl55@gmail.com>
2025-01-24 16:45:24 -0300

    feat: useNavigate function

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,12 @@
+import { useNavigate } from "react-router";
+
 export function Home() {
+  const navigate = useNavigate();
+
+  function handleProductsOpen() {
+    navigate("/products");
+  }
+
   return (
     <div>
       <h1>Home Page</h1>
@@ -6,6 +14,10 @@ export function Home() {
       <nav>
         <a href="/products">Products</a>
         <a href="/products?category=tvs">Categories</a>
+
+        <button type="button" onClick={handleProductsOpen}>
+          View Products
+        </button>
       </nav>
     </div>
   );
