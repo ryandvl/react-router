a2fccfdc02162957781a51390067e524748dcf3e
ryandvl <contatoryandvl55@gmail.com>
2025-01-24 16:48:18 -0300

    feat: route with parameter

--- src/routes/app-routes.tsx
+++ src/routes/app-routes.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router";
 import { Home } from "../pages/home";
 import { Products } from "../pages/products";
 import { NotFound } from "../pages/not-found";
+import { Details } from "../pages/details";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" index element={<Home />} />
       <Route path="/products" element={<Products />} />
+      <Route path="/details/:id" element={<Details />} />
 
       <Route path="*" element={<NotFound />} />
     </Routes>
