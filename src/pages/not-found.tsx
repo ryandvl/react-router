8e102b1441333719ae51890baf970f56dc0c8d0c
ryandvl <contatoryandvl55@gmail.com>
2025-01-24 16:33:58 -0300

    feat: add 404 page

---
+++ src/pages/not-found.tsx
@@ -0,0 +1,9 @@
+export function NotFound() {
+  return (
+    <div>
+      <h1>Oops! This page doesn't exist.</h1>
+
+      <a href="/">Back to Home Page</a>
+    </div>
+  );
+}
--- src/routes/app-routes.tsx
+++ src/routes/app-routes.tsx
@@ -2,12 +2,15 @@ import { Route, Routes } from "react-router";
 
 import { Home } from "../pages/home";
 import { Products } from "../pages/products";
+import { NotFound } from "../pages/not-found";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" index element={<Home />} />
       <Route path="/products" element={<Products />} />
+
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
