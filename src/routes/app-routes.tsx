de643bb9cad1d09725f9a980c96990b7fb4511ae
ryandvl <contatoryandvl55@gmail.com>
2025-01-24 16:24:42 -0300

    feat: products route

--- src/routes/app-routes.tsx
+++ src/routes/app-routes.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
 
 import { Home } from "../pages/home";
+import { Products } from "../pages/products";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" index element={<Home />} />
+      <Route path="/products" element={<Products />} />
     </Routes>
   );
 }
