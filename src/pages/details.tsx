8d5d5f68383fcca8ebe289530eab19fe0863b29c
ryandvl <contatoryandvl55@gmail.com>
2025-01-24 16:57:41 -0300

    feat: back to last page with params

--- src/pages/details.tsx
+++ src/pages/details.tsx
@@ -1,10 +1,14 @@
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 
 export function Details() {
   const { id } = useParams();
+  const navigate = useNavigate();
 
   return (
     <div>
+      <button type="button" onClick={() => navigate(-1)}>
+        Back
+      </button>
       <h1>Details</h1>
 
       <span>
